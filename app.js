c0ae681dcc2a9bbb43c1115331b4386147b676c8
Sango <sango.yu@chiefclouds.com>
2019-10-07 22:28:25 +0800

    add files

---
+++ app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa');
+const request = require('request');
+const Router = require('koa-router');
+const JPEGDecoder = require('jpg-stream/decoder');
+const JPEGEncoder = require('jpg-stream/encoder');
+const resize = require('resizer-stream');
+const router = new Router();
+const app = new Koa();
+
+
+router.get('*', ctx => {
+
+    let {url, width, height} = ctx.query;
+
+    let option = { allowUpscale: true };
+
+    if(width) {
+        option.width = parseInt(width);
+    }
+
+    if(height) {
+        option.height = parseInt(height);
+    }
+
+    if (!width && !height) {
+        try{
+            return ctx.body = request(url);
+        }catch(e){
+            return ctx.body = '图片url异常';
+        }
+    }
+
+    
+    try{
+        ctx.response.type = 'image/jpeg';
+        ctx.body = request(url)
+            .pipe(new JPEGDecoder)
+            .pipe(resize(option))
+            .pipe(new JPEGEncoder);
+    }catch(e) {
+        return ctx.body = '图片url异常';
+    }
+
+    
+
+});
+
+app.use(router.routes());
+
+app.listen(3000)
