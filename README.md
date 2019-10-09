babe6b5b0f904100394ac9edda3d9d5969db454e
SangoYu <sango_yu@163.com>
2019-10-09 17:09:26 +0800

    add config and update url

--- README.md
+++ README.md
@@ -5,13 +5,18 @@ Image resizer api service width nodejs.
 ### host
 http://resizer.sangolab.top/
 
+### Usage
+http://resizer.sangolab.top/{imageUrl}@{width}_{height}_{quality}q.jpg
+
 
 ### params
-`url` The url of the image to be resized.
+`imageUrl` The url of the image to be resized.
 
-[ `width` ]  (optional) Resize the image to the specific width.
+[ `width` ]  Resize the image to the specific width.
 
 [ `height` ]  (optional) Resize the image to the specific height.
+
+[ `quality` ] (optional) Resize the image to the specific quality percent, default to 80%.
 
 ### returns
 The resized image.
@@ -19,6 +24,9 @@ The resized image.
 If the image url is invalid, returns error `Image error`;
 
 ### demo
-[http://resizer.sangolab.top/?url=http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg&width=100&height=100](http://resizer.sangolab.top/?url=http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg&width=100&height=100)
-
+http://resizer.sangolab.top/http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg@50_50_80q.jpg
+http://resizer.sangolab.top/http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg@50_80q.jpg
+http://resizer.sangolab.top/http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg@50_50.jpg
+http://resizer.sangolab.top/http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg@50_100_80q.jpg
+http://resizer.sangolab.top/http://p3.music.126.net/Sv4H_U63vjguKBrRj_tUaA==/109951162839290909.jpg@100_50_80q.jpg
 
--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const Koa = require('koa');
 const request = require('request');
 const Router = require('koa-router');
-const JPEGDecoder = require('jpg-stream/decoder');
-const JPEGEncoder = require('jpg-stream/encoder');
+const pics = require('pics');
 const resize = require('resizer-stream');
+const probe = require('probe-image-size');
 const router = new Router();
 const app = new Koa();
+const config = require('./config.json');
 
+pics.use(require('gif-stream'));
+pics.use(require('jpg-stream'));
+pics.use(require('png-stream'));
 
-router.get('*', ctx => {
+router.get('/*.jpg', ctx => {
 
-    let {url, width, height} = ctx.query;
+    let url = ctx.url.slice(1);
+    
+    let dimensionStr = url.split('@');
+    url = dimensionStr[0];
+    let width, height, quality;
+
+    if (dimensionStr[1]){
+        [width, height, quality] = dimensionStr[1].split('_');
+    }
 
     let option = { allowUpscale: true };
 
@@ -18,25 +30,47 @@ router.get('*', ctx => {
         option.width = parseInt(width);
     }
 
-    if(height) {
+    if (height && height.indexOf('q')==-1){
         option.height = parseInt(height);
     }
 
-    if (!width && !height) {
-        try{
-            return ctx.body = request(url);
-        }catch(e){
-            return ctx.body = 'Image error';
-        }
+    if (quality && quality.indexOf('q') > -1) {
+        option.quality = parseInt(quality);
+    } else if (height && height.indexOf('q') > -1){
+        option.quality = parseInt(height);
     }
 
-    
     try{
-        ctx.response.type = 'image/jpeg';
-        ctx.body = request(url)
-            .pipe(new JPEGDecoder)
-            .pipe(resize(option))
-            .pipe(new JPEGEncoder({ quality: 50 }));
+        
+        let resizeStream = () => {
+            return request(url)
+                .pipe(pics.decode())
+                .pipe(resize(option))
+                .pipe(pics.encode('image/jpeg' ,{ quality: quality || config.quality }))
+        };
+
+        let urlOrStream = url;
+        let noSize = true;
+
+
+        if (width || height) {
+            urlOrStream = resizeStream();
+            noSize = false;
+        }
+        
+            
+        return probe(urlOrStream).then(res => {
+            ctx.type = 'image/jpeg';
+            ctx.set({
+                width: res.width,
+                height: res.height
+            });
+            if(noSize){
+                ctx.body = request(url);
+            }else{
+                ctx.body = resizeStream();
+            }
+        });
     }catch(e) {
         return ctx.body = 'Image error';
     }
@@ -47,4 +81,4 @@ router.get('*', ctx => {
 
 app.use(router.routes());
 
-app.listen(3000)
+app.listen(config.port)
